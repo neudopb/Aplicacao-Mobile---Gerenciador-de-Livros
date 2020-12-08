b668fe8dc8303392dd1aec4367dc4c77bce9d85a
neudinho11 <neudinho66@gmail.com>
2020-12-07 21:23:14 -0300

    Modificações no layot e inicio da implementação do SQLite

--- app/src/main/java/br/imd/fic/gerenciadordelivros/views/MainActivity.java
+++ app/src/main/java/br/imd/fic/gerenciadordelivros/views/MainActivity.java
@@ -25,13 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
         List<Livro> listaLivros = new ArrayList<>();
 
-        listaLivros.add(new Livro(1L,"Android para Leigos","Michael Burton","Alta books",false));
-        listaLivros.add(new Livro(2L,"Android para Programadores","Paul J, Deitel","Bookman",true));
-        listaLivros.add(new Livro(3L,"Desenvolvimento para Android","Griffiths, David","Alta books",false));
-        listaLivros.add(new Livro(4L,"Android Base de Dados","Queirós, Ricardo","FCA Editora",true));
-        listaLivros.add(new Livro(5L,"Android em Ação","King, Chris","Elsevier - Campus",false));
-        listaLivros.add(new Livro(6L,"Jogos em Android","Queirós, Ricardo","FCA - Editora",true));
-        listaLivros.add(new Livro(7L,"Android Essencial com Kotlin","Ricardo R.","NOVATEC",false));
+        listaLivros.add(new Livro(1L,"Android para Leigos","Michael Burton","Alta books",0));
+        listaLivros.add(new Livro(2L,"Android para Programadores","Paul J, Deitel","Bookman",1));
+        listaLivros.add(new Livro(3L,"Desenvolvimento para Android","Griffiths, David","Alta books",0));
+        listaLivros.add(new Livro(4L,"Android Base de Dados","Queirós, Ricardo","FCA Editora",1));
+        listaLivros.add(new Livro(5L,"Android em Ação","King, Chris","Elsevier - Campus",0));
+        listaLivros.add(new Livro(6L,"Jogos em Android","Queirós, Ricardo","FCA - Editora",1));
+        listaLivros.add(new Livro(7L,"Android Essencial com Kotlin","Ricardo R.","NOVATEC",0));
 
         LivroAdapter livroAdapter = new LivroAdapter(listaLivros, this);
 
